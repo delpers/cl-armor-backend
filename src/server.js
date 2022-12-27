ae67f13992e8c5468a3ebac2d8ce5aeaebd73b76
delpers <boschat@protonmail.com>
2022-12-27 15:00:45 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ var config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_SECRET,
-  baseURL: "https://admin.helpquit.io",
+  baseURL: "http://localhost:3000/",
   clientID: "s2bsIm9J9pCTmZ1xwYkr4VOIqmUgRXPu",
   issuerBaseURL: "https://helpquit.eu.auth0.com"
 }; // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -50,7 +50,7 @@ app.use("/user", authApi);
 app.get("/", function (req, res) {
   res.json({
     status: "1",
-    message: "Phlav API : https://psbp-1.osc-fr1.scalingo.io/api-docs/"
+    message: "Phlav API : https://localhost:8080/api-docs/"
   });
 });
 
@@ -58,7 +58,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/", function (req, res) {
   res.json({
     status: "1",
-    message: "Phlav API : https://psbp-1.osc-fr1.scalingo.io/api-docs/"
+    message: "Phlav API : https://localhost:8080/api-docs/"
   });
 });
 app.post("/create", function(req,res){
